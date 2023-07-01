3d560bf3e67054506662adf52b197d68905972d6
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2023-07-01 17:03:12 +0900

    first commit

---
+++ src/main/java/com/as/we/make/aswemake/AsWeMakeApplication.java
@@ -0,0 +1,15 @@
+package com.as.we.make.aswemake;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class AsWeMakeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsWeMakeApplication.class, args);
+        log.info("application activate ~~~~~~~~~~~~~~~~~~~`");
+    }
+
+}
---
+++ src/test/java/com/as/we/make/aswemake/AsWeMakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.we.make.aswemake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsWeMakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
